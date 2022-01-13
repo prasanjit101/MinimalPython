08d33d17f3c719f9922a91554943421ab23e0baa
prasanjit <prasanjitdutta45@gmail.com>
2022-01-13 09:05:04 +0530

    dice simulator working

---
+++ dice.py
@@ -0,0 +1,44 @@
+from random import randint
+print("Dice Simulator 🎲🎲")
+choice = "y"
+
+while choice == "y" or choice == "Y":
+    number = randint(1,6)
+
+    if number == 1:
+        print("----------")
+        print("|        |")
+        print("|    0   |")
+        print("|        |")
+        print("----------")
+    if number == 2:
+        print("----------")
+        print("|        |")
+        print("| O    O |")
+        print("|        |")
+        print("----------")
+    if number == 3:
+        print("----------")
+        print("|    O   |")
+        print("|    O   |")
+        print("|    O   |")
+        print("----------")
+    if number == 4:
+        print("----------")
+        print("| O    O |")
+        print("|        |")
+        print("| O    O |")
+        print("----------")
+    if number == 5:
+        print("----------")
+        print("| O    O |")
+        print("|    O   |")
+        print("| O    O |")
+        print("----------")
+    if number == 6:
+        print("----------")
+        print("| O    O |")
+        print("| O    O |")
+        print("| O    O |")
+        print("----------")
+    choice = input("To roll again ,press y : ")
